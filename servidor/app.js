82128cac69d87d7bde9303ca8575bc5ccfe4dbdc
Gustavo Tamanaka <gustavo@sflabs.com.br>
2021-03-24 13:41:39 -0300

    Cadastrar is working

--- servidor/app.js
+++ servidor/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 var admin = require("firebase-admin");
 var bodyParser = require("body-parser");
+var cors = require("cors");
 
 let credentials = `${__dirname}/credentials.json`; // Carregar as
 let firebase = admin.initializeApp({
@@ -9,6 +10,7 @@ let firebase = admin.initializeApp({
 });
 
 const db = firebase.firestore();
+app.use(cors());
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -18,36 +20,40 @@ app.use(bodyParser.json());
 
 app.post("/cadastrar", async (req, res, next) => {
   try {
-    console.log("I Worked");
-    res.status(201).send("User created");
-  } catch (error) {}
+    console.log(req.body);
+    let resp = await firebase.auth().createUser({
+      uid: (req.body.user).replace(" ", ".").toLowerCase(),
+      displayName: req.body.user,
+      password: req.body.pwd,
+      email: req.body.email,
+    });
+    console.log(resp.toJSON())
+    res.status(201).send("Usuário Criado com Sucesso");
+  } catch (error) {
+    console.log(error.errorInfo)
+    res.status(400).send(error.errorInfo.message)
+  }
 });
 
 app.post("/salvar-leituras", async (req, res, next) => {
   try {
     console.log(req.body);
-    let timestamp = req.body.timestamp
-    let content = {[timestamp]: req.body.leitura}
-    console.log(content)
-    await db
-      .collection("sensors")
-      .doc(req.body.sensor)
-      .update(content);
+    let timestamp = req.body.timestamp;
+    let content = { [timestamp]: req.body.leitura };
+    console.log(content);
+    await db.collection("sensors").doc(req.body.sensor).update(content);
     res.status(201);
   } catch (error) {
-    res.status(400).send("Failed to Update")
+    res.status(400).send("Failed to Update");
   }
 });
 
-app.post("/create-sensor", async(req,res,next)=>{
+app.post("/create-sensor", async (req, res, next) => {
   try {
-    
-  } catch (error) {
-    
-  }
-})
+  } catch (error) {}
+});
 
-app.listen(3001, async () => {
+app.listen(3002, async () => {
   console.log("Web Server Up");
   // console.log(x);
 });
